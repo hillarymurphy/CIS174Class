fa350e94ca922d51a6164e5eb809b88499d0d69d
hillary.j.murphy@gmail.com <hillary.j.murphy@gmail.com>
2020-07-10 09:58:34 -0500

    Updates for Session State and Cookies

---
+++ MovieList/Areas/OlympicGames/Controllers/FavoritesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieList.Areas.OlympicGames.Models;
+
+namespace MovieList.Areas.OlympicGames.Controllers
+{
+    [Area("OlympicGames")]
+    public class FavoritesController : Controller
+    {
+        [Route("[area]/[controller]")]
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new CountrySession(HttpContext.Session);
+            var model = new CountryListViewModel
+            {
+                ActiveSportType = session.GetActiveSportType(),
+                ActiveCat = session.GetActiveCat(),
+                Countrys = session.GetMyCountrys()
+            };
+            return View(model);
+        }
+        [Route("[area]/[controller]/Delete")]
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            var session = new CountrySession(HttpContext.Session);
+            var cookies = new CountryCookies(Response.Cookies);
+
+            session.RemoveMyCountrys();
+            cookies.RemoveMyCountryIds();
+
+            TempData["message"] = "Favorite countries cleared";
+
+            return RedirectToAction("Index", "Country",
+                new
+                {
+                    ActiveSportType = session.GetActiveSportType(),
+                    ActiveCat = session.GetActiveCat()
+                });
+        }
+    }
+}

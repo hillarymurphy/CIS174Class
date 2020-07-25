e9fc86e0456eb1fb98d7a2c2f68fe51e00831fe0
hillary.j.murphy@gmail.com <hillary.j.murphy@gmail.com>
2020-07-24 20:46:25 -0500

    Updates to add Tag Helpers and view component

---
+++ MovieList/Areas/Agile/Components/StatusFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MovieList.Areas.Agile.Models;
+
+namespace MovieList.Areas.Agile.Components
+{
+    public class StatusFilter : ViewComponent
+    {
+        private IRepository<Models.Status> data { get; set; }
+        public StatusFilter(IRepository<Status> rep) => data = rep;
+
+        public IViewComponentResult Invoke() 
+        {
+            var statuses = data.List(new QueryOptions<Status>
+            {
+                OrderBy = s => s.StatusId
+            });
+            return View(statuses);
+        }
+    }
+}
---
+++ MovieList/Areas/Agile/TagHelpers/SubmtitButtonTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Areas.Agile.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "[type=submit]")]
+    public class SubmtitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context,
+       TagHelperOutput output)
+        {
+            //make button element
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            //make a submit button
+            output.Attributes.SetAttribute("type", "submit");
+
+            //append bootstrap button classes
+            string newClasses = "btn btn-dark";
+            string oldClasses = output.Attributes["class"]?.Value?.ToString();
+            string classes = (string.IsNullOrEmpty(oldClasses)) ?
+                newClasses : $"{oldClasses} {newClasses}";
+            output.Attributes.SetAttribute("class", classes);
+
+        }
+    }
+}
+
